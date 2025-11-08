1cec240a7d2a56ea34eab9e23dbdab7352dfcfa2
LU SIYUAN <siyuanlu345@gmail.com>
2025-11-08 08:58:15 +0000

    LU SIYUAN

--- ex1.py
+++ ex1.py
@@ -1,3 +1,19 @@
+
 def find(s, n):
-# write your implementation here
+   # write your implementation here
+    seen = {}
+    
+    for i, num in enumerate(s):
+    
+        complement = n - num
+        
+       
+        if complement in seen:
+            return [seen[complement], i]
+       
+        seen[num] = i
+    
     return None
+print(find([2, 7, 11, 15], 9))  # [0, 1]
+print(find([3, 2, 4], 6))       # [1, 2]
+print(find([3, 3], 6))          # [0, 1]
